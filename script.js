0e59b2da33b186c97a02288f161e03b02046db36
Nathan <ntrumpet620@gmail.com>
2022-08-24 15:04:56 -0400

    added questions on script.js

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 var state = 'start';
 
 var startEl = document.querySelector('#start');
-var startEl = document.querySelector('#start');
-var startEl = document.querySelector('#start');
-var startEl = document.querySelector('#start');
-var startEl = document.querySelector('#start');
-var startEl = document.querySelector('#start');
-var startEl = document.querySelector('#start');
+var quizEl = document.querySelector('#quiz');
+var endEl = document.querySelector('#end');
+var startButton = document.querySelector('#start button');
+var submitScoreButton = document.querySelector('#start button');
+var quizTitle = document.querySelector('#quiz title');
+var questionsEl = document.querySelector('#questions');
+
+var questions = [
+    {
+        title: "JavaScript is a ______-side programming language",
+        possibleAnswers: ['Client', 'Server', 'Both', 'None'],
+        correct: 'Both'
+    },
+    {
+        title: "What do you put around string values?",
+        possibleAnswers: ['Quotes', 'Brackets', 'Parentheses', 'Nothing'],
+        correct: 'Quotes'
+    },
+    {
+        title: "What does Math.floor round to?",
+        possibleAnswers: ['The nearest integer', 'Rounds up to the nearest integer', 'Rounds down to the nearest integer', 'Sets the number to 0'],
+        correct: 'Rounds down to the nearest integer'
+    },
+    {
+        title: "When using .toUpperCase, what do you put at the end to execute the function?",
+        possibleAnswers: ['.toUpperCase;', '.toUpperCase();', '.toUpperCase[];', '.toUpperCase{};'],
+        correct: 'toUpperCase();'
+    },
+];
+
+var cursor = 0;
