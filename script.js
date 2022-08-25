3bfe398160f790abc2324fac163e1ad2297ba6e1
Nathan <ntrumpet620@gmail.com>
2022-08-24 20:52:03 -0400

    added functions to hide questions, and then to cycle from start to question, and then question to question

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+// base starting state for reset button
 var state = 'start';
 
+// elements needed
 var startEl = document.querySelector('#start');
 var quizEl = document.querySelector('#quiz');
 var endEl = document.querySelector('#end');
 var startButton = document.querySelector('#start button');
 var submitScoreButton = document.querySelector('#start button');
-var quizTitle = document.querySelector('#quiz title');
-var questionsEl = document.querySelector('#questions');
+var quizTitle = document.querySelector('#title');
+var optionEl = document.querySelector('#options');
 
+let indexQuestion = 0;
+// question array
 var questions = [
     {
         title: "JavaScript is a ______-side programming language",
@@ -32,3 +36,54 @@ var questions = [
 ];
 
 var cursor = 0;
+
+// starts quiz
+startButton.addEventListener('click', function () {
+    startEl.style.display = 'none';
+    quizEl.style.display = 'block';
+    displayQuestion();
+});
+
+function displayQuestion() {
+    let renderQuestion = questions[indexQuestion];
+    quizTitle.textContent = renderQuestion.title;
+    optionEl.textContent = '';
+    renderQuestion.possibleAnswers.forEach(function(choice, i){
+        let choiceButtons = document.createElement('button');
+        choiceButtons.setAttribute('value', choice);
+        choiceButtons.textContent = i + choice;
+        optionEl.onclick = nextQuestion;
+
+        optionEl.appendChild(choiceButtons);
+    });
+
+};
+
+function nextQuestion() {
+
+    if (this.value !== questions[indexQuestion].correct) {
+        window.alert( "Wrong")
+    } else {
+        window.alert('Correct')
+    };
+
+    indexQuestion++
+    displayQuestion();
+
+};
+
+// resets quiz
+function reset() {
+    state = 'start';
+};
+
+submitScoreButton.addEventListener('click', function () {
+    reset();
+});
+
+// next question
+// quizTitle.addEventListener('click', function() {
+
+// });
+// let i = 0
+// console.log(questions[i].title)
