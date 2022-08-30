3b1670e94262d91d9c893b971cd5ca145b7d8319
nathanhawk2 <ntrumpet620@gmail.com>
2022-08-29 20:11:27 -0400

    more functions added to js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// base starting state for reset button
-var state = 'start';
-
 // elements needed
 var startEl = document.querySelector('#start');
 var quizEl = document.querySelector('#quiz');
@@ -9,6 +6,8 @@ var startButton = document.querySelector('#start button');
 var submitScoreButton = document.querySelector('#start button');
 var quizTitle = document.querySelector('#title');
 var optionEl = document.querySelector('#options');
+var timeEl =document.querySelector('#time');
+var scoreEl = document.querySelector('#score');
 
 let indexQuestion = 0;
 // question array
@@ -16,12 +15,12 @@ var questions = [
     {
         title: "JavaScript is a ______-side programming language",
         possibleAnswers: ['Client', 'Server', 'Both', 'None'],
-        correct: 2
+        correct: ['Both']
     },
     {
         title: "What do you put around string values?",
         possibleAnswers: ['Quotes', 'Brackets', 'Parentheses', 'Nothing'],
-        correct: 0
+        correct: [0]
     },
     {
         title: "What does Math.floor round to?",
@@ -34,12 +33,15 @@ var questions = [
         correct: 1
     },
 ];
+console.log(questions)
 
 var cursor = 0;
 
 // starts quiz
 startButton.addEventListener('click', function () {
     startEl.style.display = 'none';
+    endEl.style.display = 'none';
+    scoreEl.style.display = 'none';
     quizEl.style.display = 'block';
     displayQuestion();
 });
@@ -48,10 +50,10 @@ function displayQuestion() {
     let renderQuestion = questions[indexQuestion];
     quizTitle.textContent = renderQuestion.title;
     optionEl.textContent = '';
-    renderQuestion.possibleAnswers.forEach(function(choice, i){
+    renderQuestion.possibleAnswers.forEach(function (choice, i) {
         let choiceButtons = document.createElement('button');
         choiceButtons.setAttribute('value', choice);
-        choiceButtons.textContent = i + choice;
+        choiceButtons.textContent = choice;
         optionEl.onclick = nextQuestion;
         optionEl.appendChild(choiceButtons);
     });
@@ -60,29 +62,50 @@ function displayQuestion() {
 
 function nextQuestion() {
 
-    if (this.value !== questions[indexQuestion].correct) {
-        window.alert( "Wrong")
+    // if (this.value !== questions.correct) {
+    //     window.alert('Wrong')
+    // } else {
+    //     window.alert('Correct')
+    // };
+    if (this.value == questions.correct) {
+        window.alert('You got it right! Good job!')
     } else {
-        window.alert('Correct')
+        window.alert('You got it wrong idiot')
     };
-
     indexQuestion++
     displayQuestion();
-
 };
+
+function correction(response) {
+    if (response){
+        alert.innerText= 'Good';
+        window.alert('');
+    } else {
+        alert.innerText = 'Bad'
+    }
+}
+
+// timer function
+var secondsLeft = 20
+function setTime() {
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft
+        if (secondsLeft === 0) {
+            window.alert('Time ran out! You fail slowpoke');
+            reset();
+        }
+
+    }, 1000);
+};
+
 
 // resets quiz
 function reset() {
-    state = 'start';
+    window.location
+    
 };
 
 submitScoreButton.addEventListener('click', function () {
     reset();
 });
-
-// next question
-// quizTitle.addEventListener('click', function() {
-
-// });
-// let i = 0
-// console.log(questions[i].title)
